6e858a0945856a5f99859c1f05f828cdff7b3a53
dUcky <ducky@dUckys-MacBook-Pro.local>
2026-03-17 17:58:13 +0800

    Remove unused template artifacts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,22 +8,15 @@ import Home from "./pages/Home";
 import CompetitorDetail from "./pages/CompetitorDetail";
 
 function Router() {
-  // make sure to consider if you need authentication for certain routes
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/competitor/:id" component={CompetitorDetail} />
       <Route path="/404" component={NotFound} />
-      {/* Final fallback route */}
       <Route component={NotFound} />
     </Switch>
   );
 }
-
-// NOTE: About Theme
-// - First choose a default theme according to your design style (dark or light bg), than change color palette in index.css
-//   to keep consistent foreground/background color across components
-// - If you want to make theme switchable, pass `switchable` ThemeProvider and use `useTheme` hook
 
 function App() {
   return (
